f3a985f8d7e9d0b9920a7ebaa7fd31e6e7c10e8a
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-19 10:56:46 +0100

    Backend Task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let taskBlockTask = true;
-    const taskBlockTask2 = false;
-    task = taskBlockTask;
-    task2 = taskBlockTask2;
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
